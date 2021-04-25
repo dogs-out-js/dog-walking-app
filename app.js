4da9ff97c7d99aafb45bee6bfeeb1c10ff75c132
Ola Kulykova <olesya.kulykova@gmail.com>
2021-04-25 13:38:59 +0300

    initial commit

--- app.js
+++ app.js
@@ -1,41 +1,35 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// ℹ️ Gets access to environment variables/settings
+// https://www.npmjs.com/package/dotenv
+require("dotenv/config");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// ℹ️ Connects to the database
+require("./db");
 
-var app = express();
+// Handles http requests (express is node js framework)
+// https://www.npmjs.com/package/express
+const express = require("express");
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// Handles the handlebars
+// https://www.npmjs.com/package/hbs
+const hbs = require("hbs");
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+const app = express();
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// ℹ️ This function is getting exported from the config folder. It runs most middlewares
+require("./config")(app);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// default value for title local
+const projectName = "dogs-out";
+const capitalized = (string) =>
+  string[0].toUpperCase() + string.slice(1).toLowerCase();
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.locals.title = `${capitalized(projectName)} created with Ironlauncher`;
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// 👇 Start handling routes here
+const index = require("./routes/index");
+app.use("/", index);
+
+// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
+require("./error-handling")(app);
 
 module.exports = app;
