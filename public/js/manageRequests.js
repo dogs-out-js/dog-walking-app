5cfa1c9b0fbabec760afd9351662d4b977952261
helenetrommelen <helenetrommelen@gmail.com>
2021-04-29 15:58:18 +0200

    created submit for accept id

--- public/js/manageRequests.js
+++ public/js/manageRequests.js
@@ -1,4 +1,4 @@
-const acceptBtns = document.querySelectorAll("#accept");
+const acceptBtns = document.querySelectorAll(".accept");
 console.log(acceptBtns);
 
 acceptBtns.forEach(button => {
@@ -7,7 +7,7 @@ acceptBtns.forEach(button => {
         // Walker.find()
         //     .then(walker => console.log("walker", walker));
         const action = 'accept';
-        const someId = event;
+        const someId = event.target;
         console.log("someId", someId);
         
     })
@@ -16,8 +16,7 @@ acceptBtns.forEach(button => {
 //find request
 //change accepted to true;
 
-//const requestId = 
-Request.findByIdAndUpdate(requestId, {accepted: accepted}, {new: true})
-        .then((updatedRequest) => {
-            console.log(updatedRequest);
-        })
+// Request.findByIdAndUpdate(requestId, {accepted: accepted}, {new: true})
+//         .then((updatedRequest) => {
+//             console.log(updatedRequest);
+//         })
